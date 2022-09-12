1066d16b5ec8b9a25c2dd16508ca864176ae3c62
Gregory Silva <gregory2278@hotmail.com>
2022-09-11 22:37:16 -0300

    fix send sms

--- backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
@@ -34,8 +34,9 @@ public class SmsService {
 		
 		String date = sale.getDate().getMonthValue() + "/" + sale.getDate().getYear();
 		
-		String msg = "O Vendedor " + sale.getSellerName() + " foi destaque em " + date
-				+ " com um total de R$ " + String.format("%.2f", sale.getAmount());
+		String msg = "O vendedor "+ sale.getSellerName();
+		msg += " foi destaque em "+date;
+		msg += " com um total de R$ "+String.format("%.2f", sale.getAmount());
 		
 		Twilio.init(twilioSid, twilioKey);
 
